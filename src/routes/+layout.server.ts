d96f6dc589dbace16d511e404891f8211b601157
cube <cube@so-icy-ent.biz>
2025-07-16 19:39:27 -0600

    Sveltekit + Supabase

---
+++ src/routes/+layout.server.ts
@@ -0,0 +1,9 @@
+import type { LayoutServerLoad } from './$types';
+
+export const load: LayoutServerLoad = async ({ locals: { safeGetSession }, cookies }) => {
+	const { session } = safeGetSession ? await safeGetSession() : { session: null };
+	return {
+		session,
+		cookies: cookies.getAll()
+	};
+};
